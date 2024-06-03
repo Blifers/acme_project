2b601caf9441feada39792ff174a1f0c38404826
Ilya Panin <pwilya2@yandex.ru>
2024-06-03 21:20:21 +0700

    task2

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -2,12 +2,20 @@ from django.shortcuts import render
 
 # Импортируем класс BirthdayForm, чтобы создать экземпляр формы.
 from .forms import BirthdayForm
+# Импортируем из utils.py функцию для подсчёта дней.
+from .utils import calculate_birthday_countdown
 
 
 def birthday(request):
     # Создаём экземпляр класса формы.
-    form = BirthdayForm()
-    # Добавляем его в словарь контекста под ключом form:
+    form = BirthdayForm(request.GET or None)
     context = {'form': form}
+    if form.is_valid():
+        birthday_countdown = calculate_birthday_countdown(
+            form.cleaned_data['birthday']
+        )
+        context.update({'birthday_countdown': birthday_countdown})
+    # Добавляем его в словарь контекста под ключом form:
+
     # Указываем нужный шаблон и передаём в него словарь контекста.
     return render(request, 'birthday/birthday.html', context)
